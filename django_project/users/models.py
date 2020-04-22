3d1285238ddad9c3ae93b21624a87912a583595b
Yamini Verma <vermayamini13@gmail.com>
2020-04-22 20:40:24 +0530

    Added requirements.txt file

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+# from PIL import Image
 
 
 class Profile(models.Model):
@@ -12,13 +12,13 @@ class Profile(models.Model):
 		return '{} Profile'.format(self.user.username)
 
 
-	def save(self, *args, **kwargs):
-		super().save(*args, **kwargs)
+	# def save(self, *args, **kwargs):
+	# 	super().save(*args, **kwargs)
 
-		img = Image.open(self.image.path)
+	# 	img = Image.open(self.image.path)
 
-		if img.height > 300 or img.width > 300:
-			output_size = (300, 300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
+	# 	if img.height > 300 or img.width > 300:
+	# 		output_size = (300, 300)
+	# 		img.thumbnail(output_size)
+	# 		img.save(self.image.path)
 
